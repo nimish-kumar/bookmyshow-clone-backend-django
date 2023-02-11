b96ff6b91ca08a9a0875fa5cfd2b2d4c7cd3836a
Nimish Kumar <nimishk642@gmail.com>
2023-02-11 20:51:37 +0530

    Installed django-environ

--- bms_backend/settings.py
+++ bms_backend/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
+env = environ.Env(DEBUG=(bool, False))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,7 +77,7 @@ WSGI_APPLICATION = 'bms_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
