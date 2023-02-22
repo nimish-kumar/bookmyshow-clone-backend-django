a66e1d9428f684341d205f0bf5a0f65f9e77d296
Nimish Kumar <nimishk642@gmail.com>
2023-02-22 12:07:10 +0530

    Changes added: - Added Facilities Tags City models to the 'meta' application - Added constants to meta application such as languages priority and states choices

---
+++ meta/constants.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Languages(models.TextChoices):
+    EN = "EN", _("English")
+    HI = "HI", _("Hindi")
+
+
+class Priority(models.IntegerChoices):
+    HI = 3
+    MD = 2
+    LO = 1
+
+
+class States(models.IntegerChoices):
+    ANDHRA_PRADESH = 1, _("Andhra Pradesh")
+    ARUNACHAL_PRADESH = 2, _("Arunachal Pradesh")
+    ASSAM = 3, _("Assam")
+    BIHAR = 4, _("Bihar")
+    CHHATTISGARH = 5, _("Chhattisgarh")
+    GOA = 6, _("Goa")
+    GUJARAT = 7, _("Gujarat")
+    HARYANA = 8, _("Haryana")
+    HIMACHAL_PRADESH = 9, _("Himachal Pradesh")
+    JHARKHAND = 10, _("Jharkhand")
+    KARNATAKA = 11, _("Karnataka")
+    KERALA = 12, _("Kerala")
+    MADHYA_PRADESH = 13, _("Madhya Pradesh")
+    MAHARASHTRA = 14, _("Maharashtra")
+    MANIPUR = 15, _("Manipur")
+    MEGHALAYA = 16, _("Meghalaya")
+    MIZORAM = 17, _("Mizoram")
+    NAGALAND = 18, _("Nagaland")
+    ODISHA = 19, _("Odisha")
+    PUNJAB = 20, _("Punjab")
+    RAJASTHAN = 21, _("Rajasthan")
+    SIKKIM = 22, _("Sikkim")
+    TAMIL_NADU = 23, _("Tamil Nadu")
+    TELANGANA = 24, _("Telangana")
+    TRIPURA = 25, _("Tripura")
+    UTTAR_PRADESH = 26, _("Uttar Pradesh")
+    UTTARAKHAND = 27, _("Uttarakhand")
+    WEST_BENGAL = 28, _("West Bengal")
+    ANDAMAN_AND_NICOBAR_ISLANDS = 29, _("Andaman and Nicobar Islands")
+    CHANDIGARH = 30, _("Chandigarh")
+    DADRA_AND_NAGAR_HAVELI_AND_DAMAN_AND_DIU = 31, _(
+        "Dadra and Nagar Haveli and Daman and Diu"
+    )
+    DELHI = 32, _("Delhi")
+    JAMMU_AND_KASHMIR = 33, _("Jammu and Kashmir")
+    LADAKH = 34, _("Ladakh")
+    LAKSHADWEEP = 35, _("Lakshadweep")
+    PUDUCHERRY = 36, _("Puducherry")
--- meta/models.py
+++ meta/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+from .constants import Priority, States
+
+
 # Create your models here.
+class Facilities(models.Model):
+    name = models.CharField(max_length=255)
+    priority = models.IntegerField(choices=Priority, default=Priority.LO)
+
+    class Meta:
+        ordering = ["-id"]
+        indexes = [
+            models.Index(
+                name="facilities_name_idx",
+                fields=[
+                    "name",
+                ],
+            )
+        ]
+
+
+class Tags(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["-id"]
+
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    state = models.IntegerField(choices=States.choices)
