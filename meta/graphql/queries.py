6a26017c1c8b3d47ab59e9bb109e92ffaaf5ab93
Nimish Kumar <nimishk642@gmail.com>
2023-04-16 13:57:34 +0530

    Added query for fetching all cities

--- meta/graphql/queries.py
+++ meta/graphql/queries.py
@@ -1,12 +1,7 @@
 import graphene
 from .types import CityType
-from ..models import City
+from .resolvers import CityResolver
 
 
 class MetaQuery(graphene.ObjectType):
-    list_cities = graphene.List(CityType)
-    # list_bookings_by_movies_and_date = graphene.List()
-
-    def resolve_list_movies_by_city(root, info, **kwargs):
-        # Querying a list
-        return City.objects.all()
+    list_cities = graphene.List(CityType, resolver=CityResolver())
