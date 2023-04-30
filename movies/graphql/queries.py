9ec6cd9994bd62d0c8091ecf0d90e9c7c6386bac
Nimish Kumar <nimishk642@gmail.com>
2023-04-30 21:34:18 +0530

    CLEANUP: Removed commented code

--- movies/graphql/queries.py
+++ movies/graphql/queries.py
@@ -78,61 +78,6 @@ class MoviesQuery(graphene.ObjectType):
         ),
     )
 
-    # @login_required
-    # def resolve_list_movie_lang_by_city(
-    #     root,
-    #     info,
-    #     city,
-    # ):
-    #     movie_langs = list(
-    #         BookingSlot.objects.select_related("screen__theatre__city")
-    #         .filter(
-    #             screen__theatre__city_id=city,
-    #             is_fully_booked=False,
-    #             screening_datetime__gte=timezone.now(),
-    #             screening_datetime__lte=timezone.now() + timedelta(days=7),
-    #         )
-    #         .order_by()
-    #         .values_list("movie", "lang", "format")
-    #         .distinct()
-    #     )
-    #     movie_details_dict = dict()
-    #     for movie_lang in movie_langs:
-    #         movie_id, lang_id, format_id = movie_lang
-    #         if movie_id not in movie_details_dict.keys():
-    #             movie_details_dict[movie_id] = {lang_id: set([format_id])}
-    #         else:
-    #             movie_details_dict[movie_id][lang_id].add(format_id)
-
-    #     movies_details_list = []
-    #     for movie_id in movie_details_dict.keys():
-    #         movies_details_list.append({"movie": movie_id, "langs": list()})
-
-    #     for movie in movies_details_list:
-    #         [
-    #             movie["langs"].append(
-    #                 {
-    #                     "lang": lang_id,
-    #                     "formats": list(
-    #                         movie_details_dict[movie["movie"]][lang_id]
-    #                     ),
-    #                 }
-    #             )
-    #             for lang_id in movie_details_dict[movie["movie"]].keys()
-    #         ]
-
-    #     for movie_detail in movies_details_list:
-    #         movie_detail["movie"] = Movie.objects.get(pk=movie_detail["movie"])
-    #         for lang_detail in movie_detail["langs"]:
-    #             lang_detail["lang"] = Language.objects.get(
-    #                 pk=lang_detail["lang"]
-    #             )
-    #             lang_detail["formats"] = MovieFormat.objects.filter(
-    #                 id__in=lang_detail["formats"]
-    #             )
-
-    #     return movies_details_list
-
     @login_required
     def resolve_list_movie_slots_by_city_date_lang(
         root, info, city, language, movie, format
