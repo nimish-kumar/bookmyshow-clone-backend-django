15700e29c68792aa5725d8c4860bf83cf3cc3499
Nimish Kumar <nimishk642@gmail.com>
2023-05-01 21:30:19 +0530

    Added GET_USER_DETAILS query

--- core/graphql/schema.py
+++ core/graphql/schema.py
@@ -4,9 +4,10 @@ from movies.graphql.queries import MoviesQuery
 from movies.graphql.mutations import Mutation as MoviesMutation
 from core.graphql.mutations import Mutation as CoreMutation
 from meta.graphql.queries import MetaQuery
+from core.graphql.queries import Query as CoreQuery
 
 
-class Query(MetaQuery, MoviesQuery, graphene.ObjectType):
+class Query(MetaQuery, MoviesQuery, CoreQuery, graphene.ObjectType):
     pass
 
 
