be0a6287196dbf21c6438094511299ad6c4bc404
Nimish Kumar <nimishk642@gmail.com>
2023-05-02 20:16:03 +0530

    Added email to UserType

--- core/graphql/types.py
+++ core/graphql/types.py
@@ -1,7 +1,8 @@
 from graphene_django import DjangoObjectType
 from ..models import User
 
+
 class UserType(DjangoObjectType):
     class Meta:
-        model= User
-        fields = ["first_name", "last_name", "profile_image_url"]
+        model = User
+        fields = ["first_name", "last_name", "email", "profile_image_url"]
